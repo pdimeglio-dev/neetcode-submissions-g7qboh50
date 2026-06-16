c22b45f114acd30ecb9ac875a66b1225ad3515e6
pdimeglio-dev <dimeglio.pablo@gmail.com>
2026-06-16 12:23:00 -0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+            
+            if (map.has(complement)) {
+                return [i, map.get(complement)];
+            } else {
+                map.set(target - complement, i);
+            }
+        }
+    }
+}
